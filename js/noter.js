9ca4d190d9976cc5360a65b1483629994f5b2e0d
Riiskager <emil.axel.riis@gmail.com>
2024-09-30 12:40:15 +0200

    Top navigation + link til ekstern side

---
+++ js/noter.js
@@ -0,0 +1,19 @@
+
+
+let a = "Bingo";
+console.log(header)
+
+let header = "<h1>" + a + "</h1>";
+
+document.getElementById("hej").innerHTML = header;
+document.getElementById("hej").style.color = "blue";
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
